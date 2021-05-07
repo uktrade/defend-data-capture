14b60f18744d83a9b91335950f746a4f4d5b744d
Miriam Forner <mforner13@gmail.com>
2021-05-07 15:51:46 +0100

    Add allowed hosts setting

--- defend_data_capture/defend_data_capture/settings.py
+++ defend_data_capture/defend_data_capture/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY", default="secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 # Application definition
